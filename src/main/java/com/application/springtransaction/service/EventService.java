5c3b7dfd64a64f41628fab52392c90b82ef49489
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-11-11 17:34:36 +0900

    feat(service) : 모든 이벤트 조회 API 개발

--- src/main/java/com/application/springtransaction/service/EventService.java
+++ src/main/java/com/application/springtransaction/service/EventService.java
@@ -2,19 +2,27 @@ package com.application.springtransaction.service;
 
 import com.application.springtransaction.domain.Event;
 import com.application.springtransaction.dto.event.EventRequestDto;
+import com.application.springtransaction.dto.event.EventResponseDto;
+import com.application.springtransaction.mapper.EventMapper;
 import com.application.springtransaction.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
 
-    public Event createEvent(EventRequestDto eventRequestDto) {
-        Event event = Event.builder()
-                .name(eventRequestDto.getName())
-                .totalStock(eventRequestDto.getTotalStock()).build();
-        return eventRepository.save(event);
+    public EventResponseDto createEvent(EventRequestDto eventRequestDto) {
+        Event savedEvent = eventRepository.save(eventMapper.toEntity(eventRequestDto));
+        return eventMapper.toDto(savedEvent);
+    }
+
+    public List<EventResponseDto> getAllEvents() {
+        List<Event> events = eventRepository.findAll();
+        return events.stream().map(eventMapper::toDto).toList();
     }
 }
