21159e60fb178f7ce0cef55d4fcf8bc16eeb0b2d
nayoung16 <nykim1016@sookmyung.ac.kr>
2025-11-14 18:06:17 +0900

    feat(controller) : 티켓 구매 API 개발

--- src/main/java/com/application/springtransaction/controller/EventController.java
+++ src/main/java/com/application/springtransaction/controller/EventController.java
@@ -3,7 +3,9 @@ package com.application.springtransaction.controller;
 import com.application.springtransaction.domain.Event;
 import com.application.springtransaction.dto.event.EventRequestDto;
 import com.application.springtransaction.dto.event.EventResponseDto;
+import com.application.springtransaction.dto.purchase.PurchaseRequestDto;
 import com.application.springtransaction.service.EventService;
+import com.application.springtransaction.service.PurchaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import java.util.List;
 public class EventController {
 
     private final EventService eventService;
+    private final PurchaseService purchaseService;
 
     @PostMapping
     public ResponseEntity<EventResponseDto> createEvent(@RequestBody EventRequestDto eventRequestDto) {
@@ -34,5 +37,12 @@ public class EventController {
     public ResponseEntity<EventResponseDto> getEvent(@PathVariable Long eventId) {
         EventResponseDto eventById = eventService.getEventById(eventId);
         return ResponseEntity.status(HttpStatus.OK).body(eventById);
+    }
+
+    @PostMapping("/{eventId}/purchase")
+    public ResponseEntity<?> purchaseEvent(@PathVariable Long eventId, @RequestBody PurchaseRequestDto purchaseRequestDto) {
+        purchaseService.savePurchase(purchaseRequestDto, eventId);
+        eventService.deductStock(eventId, purchaseRequestDto.getQuantity());
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
